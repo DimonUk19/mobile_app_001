497e1ae5a4f3d37fb74ab8c126cc0085e76a7370
DimonUk19 <66688737+DimonUk19@users.noreply.github.com>
2021-06-29 00:26:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,202 @@
-'''
-Canvas stress
-=============
-
-This example tests the performance of our Graphics engine by drawing large
-numbers of small squares. You should see a black canvas with buttons and a
-label at the bottom. Pressing the buttons adds small colored squares to the
-canvas.
-
-'''
-
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.graphics import Color, Rectangle
-from random import random as r
-from functools import partial
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+# set window size
+Window.size = (300, 450)
+# load the string for design
+Builder.load_string("""
+<CalLayout>
+    BoxLayout:
+        orientation: "vertical"
+        size: root.width, root.height
+        TextInput:
+            id:input
+            text:"0"
+            halign:"right"
+            font_size:60
+            size_hint:(1, .20)
+        GridLayout:
+            cols:4
+            rows:5
+            # first row
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"C"
+                on_press:root.clear()
+            Button:
+                size_hint:(.2, .2)
+                font_size:27
+                text:"TNGA"
+                on_press:root.new()
+            
+            Button:
+                size_hint:(.2, .2)
+                font_size:27
+                text:"ZR"
+                on_press:root.new_zr()                
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"/"
+                on_press:root.pressed('/')
+            # second row
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"7"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(7)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"8"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(8)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"9"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(9)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"-"
+                on_press:root.pressed('-')
+            # third row
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"4"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(4)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"5"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(5)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"6"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(6)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"+"
+                on_press:root.pressed('+')
+            # fourth row
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"1"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(1)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"2"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(2)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"3"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(3)
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"="
+                on_press:root.answer()
+            # fifth row
+            
+            Button:
+                size_hint:(.2, .2)
+                font_size:32
+                text:"0"
+                background_color:(157/255,157/255, 157/255, 1)
+                on_press:root.pressed(0)
+            
+            
+""")
 
 
-class StressCanvasApp(App):
+# Create a class for calculator layout
+class CalLayout(Widget):
 
-    def add_rects(self, label, wid, count, *largs):
-        label.text = str(int(label.text) + count)
-        with wid.canvas:
-            for x in range(count):
-                Color(r(), 1, 1, mode='hsv')
-                Rectangle(pos=(r() * wid.width + wid.x,
-                               r() * wid.height + wid.y), size=(20, 20))
+    def new(self):
+        expression = self.ids.input.text
+        expression = int(expression)
+        expression = expression * 14
+        while expression % 4 != 0:
+            expression += 1
+        self.ids.input.text = str(expression)
 
-    def double_rects(self, label, wid, *largs):
-        count = int(label.text)
-        self.add_rects(label, wid, count, *largs)
+    def new_zr(self):
+        expression = self.ids.input.text
+        expression = int(expression)
+        expression = expression * 16
+        if expression % 4 == "0":
+            self.ids.input.text = str(expression)
+        else:
+            self.ids.input.text = str(expression + 2)
+    # function to clear text field
+    def clear(self):
+        self.ids.input.text = ""
 
-    def reset_rects(self, label, wid, *largs):
-        label.text = '0'
-        wid.canvas.clear()
+    # function to delete last number
+    # from text field
+    def back(self):
+        expression = self.ids.input.text
+        expression = expression[:-1]
+        self.ids.input.text = expression
 
+    # function take button inuputs pressed
+    # by user
+    def pressed(self, button):
+        # expression to store all text field values
+        expression = self.ids.input.text
+        # if text field expression contains
+        # error then set it to empty field
+        if "Error" in expression:
+            expression = ""
+        # if text filed expression contains
+        # 0 then first set the field to empty and
+        # display the button text pressed by user
+        if expression == "0":
+            self.ids.input.text = ""
+            self.ids.input.text = f"{button}"
+        else:
+            self.ids.input.text = f"{expression}{button}"
+
+    def answer(self):
+        expression = self.ids.input.text
+        try:
+            # evaluate text field expression
+            # using eval() function
+            self.ids.input.text = str(eval(expression))
+
+        except:
+            # set text field to Error if
+            # try block throws an error
+            self.ids.input.text = "Error"
+        # class to create calculator app
+
+
+class CalculatorApp(App):
+    # function to build app layout
     def build(self):
-        wid = Widget()
-
-        label = Label(text='0')
-
-        btn_add100 = Button(text='+ 100 rects',
-                            on_press=partial(self.add_rects, label, wid, 100))
-
-        btn_add500 = Button(text='+ 500 rects',
-                            on_press=partial(self.add_rects, label, wid, 500))
-
-        btn_double = Button(text='x 2',
-                            on_press=partial(self.double_rects, label, wid))
-
-        btn_reset = Button(text='Reset',
-                           on_press=partial(self.reset_rects, label, wid))
-
-        layout = BoxLayout(size_hint=(1, None), height=50)
-        layout.add_widget(btn_add100)
-        layout.add_widget(btn_add500)
-        layout.add_widget(btn_double)
-        layout.add_widget(btn_reset)
-        layout.add_widget(label)
-
-        root = BoxLayout(orientation='vertical')
-        root.add_widget(wid)
-        root.add_widget(layout)
-
-        return root
+        return CalLayout()
 
 
-if __name__ == '__main__':
-    StressCanvasApp().run()
+# create an object of class
+cal = CalculatorApp()
+# run the app using object
+cal.run()
